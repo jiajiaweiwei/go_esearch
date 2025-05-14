6c1ef03ac176fe4b1a32f566b55380602bc10e6c
ljw2088 <118275178+ljw2088@users.noreply.github.com>
2025-05-14 21:58:48 +0800

    倒æ位图开始

--- my_test/main/main.go
+++ my_test/main/main.go
@@ -1,11 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func main() {
-	m := make(map[string]string)
-	m["1"] = "1"
-	value, ok := m["2"]
+	var test sync.Locker
+	test = &sync.Mutex{}
+	mutex, ok := test.(*sync.RWMutex)
 	if !ok {
-		println("nothing.")
+		panic("test")
 	}
-	println(value)
+	mutex.Lock()
 }
+
+func main2() {
+	defer func() {
+		fmt.Println("First defer starts")
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in first defer:", r)
+		} else {
+			fmt.Println("First defer: no panic to recover")
+		}
+		fmt.Println("First defer ends")
+	}()
+
+	defer func() {
+		fmt.Println("Second defer starts")
+		// 这里覆盖了第一个panic
+		panic("new panic from second defer")
+		fmt.Println("Second defer ends") // 不会执行
+	}()
+
+	panic("original panic")
+	fmt.Println("After original panic") // 不会执行
+}
+
+/*
+执行结果
+Second defer starts
+First defer starts
+Recovered in first defer: new panic from second defer
+First defer ends
+panic : original panic
+
+*/
